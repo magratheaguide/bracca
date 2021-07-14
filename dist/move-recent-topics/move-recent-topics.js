795e869239023c7dc655fc9bc49a1b6a91ecaaac
thewildmage <cecilia@hey.com>
2021-07-13 19:35:49 -0600

    extract RECENT_TOPICS_PARENT_ID for use in error messaging

--- dist/move-recent-topics/move-recent-topics.js
+++ dist/move-recent-topics/move-recent-topics.js
@@ -1,13 +1,19 @@
 (function () {
+    const RECENT_TOPICS_PARENT_ID = "js-recent-topics-parent";
+
     const RECENT_TOPICS = document.getElementById("recent-topics");
-    const RECENT_TOPICS_PARENT = document.getElementById("js-recent-topics-parent");
+    const RECENT_TOPICS_PARENT = document.getElementById(
+        RECENT_TOPICS_PARENT_ID
+    );
 
     if (!RECENT_TOPICS) {
         console.error("#recent-topics could not be found");
     }
 
     if (!RECENT_TOPICS_PARENT) {
-        console.error(`New recent topics parent could not be found, expected element with ID ${RECENT_TOPICS_PARENT_ID}`);
+        console.error(
+            `New recent topics parent could not be found, expected element with ID ${RECENT_TOPICS_PARENT_ID}`
+        );
     }
 
     if (RECENT_TOPICS && RECENT_TOPICS_PARENT) {
