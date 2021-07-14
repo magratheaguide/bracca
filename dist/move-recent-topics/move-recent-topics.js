fcc55f16ebd5caf721cefa2e34bf3769f6386cb3
thewildmage <cecilia@hey.com>
2021-07-13 19:37:51 -0600

    Move recent topics (#1)
    * tweak wording in readme and license to be more clear
    
    * add pre-commit and prettier config, add CODEOWNERS
    
    * add move-recent-topics.js and its instructions
    
    * extract RECENT_TOPICS_PARENT_ID for use in error messaging
    
    * add badges to readme

---
+++ dist/move-recent-topics/move-recent-topics.js
@@ -0,0 +1,22 @@
+(function () {
+    const RECENT_TOPICS_PARENT_ID = "js-recent-topics-parent";
+
+    const RECENT_TOPICS = document.getElementById("recent-topics");
+    const RECENT_TOPICS_PARENT = document.getElementById(
+        RECENT_TOPICS_PARENT_ID
+    );
+
+    if (!RECENT_TOPICS) {
+        console.error("#recent-topics could not be found");
+    }
+
+    if (!RECENT_TOPICS_PARENT) {
+        console.error(
+            `New recent topics parent could not be found, expected element with ID ${RECENT_TOPICS_PARENT_ID}`
+        );
+    }
+
+    if (RECENT_TOPICS && RECENT_TOPICS_PARENT) {
+        RECENT_TOPICS_PARENT.appendChild(RECENT_TOPICS);
+    }
+})();
