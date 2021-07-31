05cc41009da48c0dee47e0aec49b318019ee05fc
thewildmage <cecilia@hey.com>
2021-07-31 13:52:26 -0600

    remove debug logging

--- dist/wrap-first-last-word/wrap-first-last-word.js
+++ dist/wrap-first-last-word/wrap-first-last-word.js
@@ -70,7 +70,6 @@ THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLI
                     let mode = modes[selectedMode];
 
                     mode.configureWord(allWords);
-                    console.debug(mode);
 
                     let node = findContainingNode(
                         target,
