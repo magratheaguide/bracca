130288cc7559e90d7ba928d863b58a5557c198ac
thewildmage <cecilia@hey.com>
2021-08-04 20:28:30 -0600

    Add wrap word script (#4)
    * first pass at invisible and embedded tag aware script, with tests
    
    * more comprehensive tests
    
    * remove developmental console logging statements
    
    * add multiple same words test
    
    * full refactor to remove extra steps
    
    * update injectWrapper() to handle last word as well
    
    * add tests for first word only and last word only
    
    * make the test page a complete, valid html page
    
    * add do not wrap test
    
    * add test for missing word position
    
    * consider word position specification
    
    * rewrite again to improve extensibility and consistency
    
    * update classes and data attributes to match rewrite, add test for unsupported mode
    
    * remove debug logging
    
    * make file names less specific
    
    * simplify
    
    * Create README.md
    
    * add info about test file to readme
    
    * add htmlhint config, fix html linter error

---
+++ dist/wrap-word/README.md
@@ -0,0 +1,42 @@
+# Wrap Word
+
+## Objective
+
+Dynamically wrap a specific word within an element so that it can be individually styled. Supports first and last words.
+
+## How to Use
+
+1. Load the script by adding `<script src="https://coding-camp-wiki.github.io/jcink-utility-scripts/dist/word-wrap/word-wrap.js" defer></script>` to the site's HTML
+    - We recommend that you add this script to the specific pages it'll be used on, as high up on that page as possible. Ideally, within the `<head>` tag. (Note that this script does **not** need to be placed just before the closing `</body>` tag [thanks to the `defer` keyword](https://developer.mozilla.org/en-US/docs/Learn/JavaScript/First_steps/What_is_JavaScript#script_loading_strategies).)
+1. Specify the parent elements containing words that should be wrapped by adding `data-trigger-word-wrap="first last"`, removing any modes that you do not want to use
+1. Add CSS styles. Words will be wrapped in spans which will all use the class `js-wrapped-word`. Based on which word is wrapped, there will be an additional class like `--first-word`
+
+## Before and After
+
+If you start with the following HTML:
+
+```html
+<p data-trigger-word-wrap="first">This is a demo.</p>
+```
+
+Then after this script runs, you'll end up with:
+
+```html
+<p data-trigger-word-wrap="first">
+    <span class="js-wrapped-word --first-word">This</span> is a demo.
+</p>
+```
+
+The following CSS would then cause that first word ("This") to have a pink background:
+
+```css
+.js-wrapped-word.--first-word {
+    background: pink;
+}
+```
+
+Note that all word wrappers share a common class and then have additional modifier classes based on the word's position. This enables you to style all the wrapped words the same, or to style first and last words differently from each other, or some mix thereof.
+
+## Testing the Script
+
+A [`_test.html`](/_test.html) is included herein. This is not meant to be used on your site. It's for testing changes to the script to make sure that it still works as intended in a variety of possible circumstances.
---
+++ dist/wrap-word/wrap-word.js
@@ -0,0 +1,153 @@
+/*
+MIT License
+
+Modified work copyright (c) 2021 Coding Camp and other contributors
+Original work copyright (c) 2021 by Mario Vidov (https://codepen.io/mel/pen/jLEKH)
+
+Permission is hereby granted, free of charge, to any person obtaining a copy of this software and associated documentation files (the "Software"), to deal in the Software without restriction, including without limitation the rights to use, copy, modify, merge, publish, distribute, sublicense, and/or sell copies of the Software, and to permit persons to whom the Software is furnished to do so, subject to the following conditions:
+The above copyright notice and this permission notice shall be included in all copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+*/
+
+"use strict";
+
+(function () {
+    class Mode {
+        constructor(index, wrapperClass) {
+            this.index = index;
+            this.wrapperClass = `${sharedWrapperClass} ${wrapperClass}`;
+
+            if (index >= 0) {
+                this.isForwardSearching = true;
+            } else {
+                this.isForwardSearching = false;
+            }
+        }
+    }
+
+    const triggerSelector = "[data-trigger-word-wrap]";
+    const triggerJs = "triggerWordWrap";
+
+    const targets = document.querySelectorAll(triggerSelector);
+
+    const sharedWrapperClass = "js-wrapped-word";
+    const modes = {
+        first: new Mode(0, "--first-word"),
+        last: new Mode(-1, "--last-word"),
+    };
+
+    for (const target of targets) {
+        const selectedModes = target.dataset[triggerJs].trim().split(/\s+/);
+
+        for (const selectedMode of selectedModes) {
+            if (modes.hasOwnProperty(selectedMode)) {
+                const mode = modes[selectedMode];
+                const word = getWord(target, mode);
+                const wordWrapped = wrapWord(word, mode);
+                const textNode = getContainingTextNode(
+                    target,
+                    word,
+                    mode.isForwardSearching
+                );
+
+                injectWrapper(
+                    textNode,
+                    word,
+                    wordWrapped,
+                    mode.isForwardSearching
+                );
+            } else if (selectedMode === "") {
+                console.error(
+                    `Found empty trigger on %o
+At least one mode must be specified, options are: %o`,
+                    target,
+                    Object.keys(modes)
+                );
+            } else {
+                console.error('Unsupported mode "%s" found', selectedMode);
+            }
+        }
+    }
+
+    function getWord(parent, mode) {
+        const viewableContent = parent.innerText;
+
+        if (/\S/.test(viewableContent)) {
+            const words = viewableContent.trim().split(/\s+/);
+
+            if (mode.isForwardSearching) {
+                return words[mode.index];
+            } else {
+                return words[words.length + mode.index];
+            }
+        } else {
+            console.warn(
+                "No visible, non-whitespace content in targeted element %o",
+                parent
+            );
+        }
+    }
+
+    function wrapWord(word, mode) {
+        return `<span class="${mode.wrapperClass}">${word}</span>`;
+    }
+
+    function getContainingTextNode(parent, searchFor, isForwardSearching) {
+        let childNodes = Array.from(parent.childNodes);
+
+        if (!isForwardSearching) {
+            childNodes.reverse();
+        }
+
+        for (const child of childNodes) {
+            const searchForRegExp = new RegExp(searchFor);
+
+            if (
+                searchForRegExp.test(child.nodeValue) ||
+                searchForRegExp.test(child.innerText)
+            ) {
+                // prefer nodeName over nodeType for readability
+                if (child.nodeName === "#text") {
+                    return child;
+                } else {
+                    return getContainingTextNode(
+                        child,
+                        searchFor,
+                        isForwardSearching
+                    );
+                }
+            }
+        }
+
+        return;
+    }
+
+    function injectWrapper(node, word, wordWrapped, isForwardSearching) {
+        /*
+        lots of helpful technique advice for this bit:
+        https://stackoverflow.com/questions/16662393/insert-html-into-text-node-with-javascript#29301739
+        */
+
+        let newNodeValue;
+
+        if (isForwardSearching) {
+            newNodeValue = node.nodeValue.replace(word, wordWrapped);
+        } else {
+            const index = node.nodeValue.lastIndexOf(word);
+            const valueStart = node.nodeValue.substring(0, index);
+            let valueEnd = node.nodeValue.substring(index);
+
+            valueEnd = valueEnd.replace(word, wordWrapped);
+            newNodeValue = valueStart + valueEnd;
+        }
+
+        const tempElement = document.createElement("div");
+
+        node.parentNode.insertBefore(tempElement, node);
+        tempElement.insertAdjacentHTML("afterend", newNodeValue);
+
+        tempElement.remove();
+        node.remove();
+    }
+})();
