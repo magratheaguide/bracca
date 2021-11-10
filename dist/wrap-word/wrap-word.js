412503bfca0e6557c420fc2a3f41140575a6cf6b
thewildmage <cecilia@hey.com>
2021-11-10 09:22:44 -0700

    clarify copyrights

--- dist/wrap-word/wrap-word.js
+++ dist/wrap-word/wrap-word.js
@@ -1,7 +1,7 @@
 /*
 MIT License
 
-Modified work copyright (c) 2021 Coding Camp and other contributors
+Modified work copyright (c) 2021 Coding Camp
 Original work copyright (c) 2021 by Mario Vidov (https://codepen.io/mel/pen/jLEKH)
 
 Permission is hereby granted, free of charge, to any person obtaining a copy of this software and associated documentation files (the "Software"), to deal in the Software without restriction, including without limitation the rights to use, copy, modify, merge, publish, distribute, sublicense, and/or sell copies of the Software, and to permit persons to whom the Software is furnished to do so, subject to the following conditions:
